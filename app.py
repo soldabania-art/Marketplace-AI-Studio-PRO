18ef2b57793e7e1090cd723b90199c5aea75e38a
soldabania-art <soldabania@gmail.com>
2026-09-06 14:41:02 +0400

    Launch stability audit UI 3.4

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from studio.diagnostics import setup_logging, install_exception_hook
 setup_logging()
 install_exception_hook()
 
-from studio.ui_v33 import run
+from studio.ui_v34 import run
 
 if __name__ == '__main__':
     run()

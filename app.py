c149e08afa82f636da4a241417472accd25eb93a
soldabania-art <soldabania@gmail.com>
2026-09-06 13:29:32 +0400

    Start Mega Edition 2.6 with crash logging

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from studio.ui_v25 import run
+from studio.diagnostics import setup_logging, install_exception_hook
+
+setup_logging()
+install_exception_hook()
+
+from studio.ui_v26 import run
 
 if __name__ == '__main__':
     run()

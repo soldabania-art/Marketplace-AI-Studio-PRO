ba27e76b26099ba05b8f80e1252a94b14aaa5d1b
soldabania-art <soldabania@gmail.com>
2026-09-07 10:54:18 +0400

    Fix FBO watch backend helper import

--- web/app/api/marketplace/fbo-watch/route.js
+++ web/app/api/marketplace/fbo-watch/route.js
@@ -1,6 +1,6 @@
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
-import { backendRequest } from '../../../../../lib/backend'
+import { backendRequest } from '../../../../lib/backend'
 
 export async function POST(request){
   const store=await cookies()
